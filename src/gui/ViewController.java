d790de23bdfd2a8ecac4714fc9cd4f240c30479e
NitroRiser <jonathanfariastech@gmail.com>
2022-08-29 16:44:18 +0000

    text fields and labels

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -4,14 +4,32 @@ import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
-	@FXML
-	private Button btMyButton;
-	
-	@FXML
-	public void onMyButtonClick() {
-		Alerts.showAlert("Alert Title", null, "Hello", AlertType.ERROR);
+    @FXML
+    private TextField txtNumber1;
+
+    @FXML
+    private TextField txtNumber2;
+
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+	public void onBtSumClick() {
+        try{
+		double number1 = Double.parseDouble(txtNumber1.getText());
+        double number2 = Double.parseDouble(txtNumber2.getText());
+        double sum = number1 + number2;
+        labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e){
+            Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+        }
 	}
 }
