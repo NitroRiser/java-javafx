49dd0da1e857320c4b3bc3852837ebc955df64a4
NitroRiser <jonathanfariastech@gmail.com>
2022-08-29 13:57:50 +0000

    treating events fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application{
 
     public void start(Stage stage){
         try{
-            Parent parent = FXMLLoader.load(getClass().getResource("/gui/view.fxml"));
+            Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
             Scene scene = new Scene(parent);
             stage.setScene(scene);
             stage.show();
