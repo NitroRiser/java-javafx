0765399c519f09472a516de5c4c133639d12fb35
NitroRiser <jonathanfariastech@gmail.com>
2022-08-29 13:43:04 +0000

    javafx fxml

---
+++ src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Main
+ */
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage){
+        try{
+            Parent parent = FXMLLoader.load(getClass().getResource("/gui/view.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
